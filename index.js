f726361cf68b9c10e82c74979471fa3597429623
Jay P Hodges <jayphodges@gmail.com>
2022-10-25 17:12:07 -0600

    commit tree bd834cc0292bd1eafdb21de189e0765f3ee75833

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(item => item.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(array, name) {
+    return array.filter(item => item.slice(0, name.length) === name)
+}
+function matchName(array, name) {
+    return array.filter(item => item.name === name)
+}
